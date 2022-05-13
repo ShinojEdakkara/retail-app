b0e978d9923923bd4f07bba651fb3f6ed41d2d31
Shinoj Zacharias <shinojedakkara@gmail.com>
2022-05-13 10:12:01 +0530

    Initial commit

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this runs each time the container starts
+
+echo "post-start start"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start start" >> "$HOME/status"
+
+sudo mount -a
+git -C ../template pull
+git -C ../gitops pull
+
+echo "post-start complete"
+echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
